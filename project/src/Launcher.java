8890e94031e258162f4ffce72cee10411f0bbce1
AbdulHadi117 <117abdulhadi@gmail.com>
2026-02-15 16:13:16 +0500

    Application Launcher

--- project/src/Launcher.java
+++ project/src/Launcher.java
@@ -1,3 +1,15 @@
+import app.App;
+
 public class Launcher {
-    
+
+    /**
+     * The main method serves as the entry point for the application.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // Suppress unused warning for this instance
+        @SuppressWarnings("unused")
+        App app = new App(); // Initialize the application
+    }
 }
